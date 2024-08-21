f8a865a8ab919fdb3caa45eaa1bfe6981090ae00
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-21 16:07:26 -0500

    Program created

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿using System;
+
+namespace Exercise1
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int age;
+            int favoriteNumber;
+
+            Console.Write("What is your first name? ");
+            firstName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            lastName = Console.ReadLine();
+
+            Console.Write("What is your age? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is your favorite number? ");
+            favoriteNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + firstName + " " + lastName +
+                "! You are " + age + " years old and your favorite number is " + favoriteNumber + ".");
+        }
     }
 }
