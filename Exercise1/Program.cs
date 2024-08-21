0d1a35ae8bc13fa427e165e2f663e191aa134660
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-21 16:49:56 -0500

    updated comments

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,25 +6,25 @@ namespace Exercise1
     {
         private static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            int age;
-            int favoriteNumber;
+            string firstName; //declares firstName variable
+            string lastName; //declares lastName variable
+            int age; //declares age variable
+            int favoriteNumber; //declares favoriteNumber variable
 
-            Console.Write("What is your first name? ");
-            firstName = Console.ReadLine();
+            Console.Write("What is your first name? "); //prompts the user for input
+            firstName = Console.ReadLine(); //initializes firstName variable with user input
 
-            Console.Write("What is your last name? ");
-            lastName = Console.ReadLine();
+            Console.Write("What is your last name? "); //prompts the user for input
+            lastName = Console.ReadLine(); //initializes lastName variable with user input
 
-            Console.Write("What is your age? ");
-            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is your age? "); //prompts the user for input
+            age = Convert.ToInt32(Console.ReadLine()); //converts and initializes user input into age variable
 
-            Console.Write("What is your favorite number? ");
-            favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is your favorite number? "); //prompts the user for input
+            favoriteNumber = Convert.ToInt32(Console.ReadLine()); //converts and initializes user input into favoriteNumber variable
 
             Console.WriteLine("Hello, " + firstName + " " + lastName +
-                "! You are " + age + " years old and your favorite number is " + favoriteNumber + ".");
+                "! You are " + age + " years old and your favorite number is " + favoriteNumber + "."); //outputs combined variables into statement
         }
     }
 }
